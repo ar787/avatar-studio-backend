0cb355bb61ad65a7a2c5f20dc106fdb2113961c6
ar787 <armanhovhannisyan1002@gmail.com>
2026-02-13 12:35:06 +0400

    add firebase emulator

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,12 +6,14 @@ type Config = {
   port: number;
   nodeEnv: string;
   geminiApiKey: string;
+  firestoreEmulator: string;
 };
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || 'development',
   geminiApiKey: process.env.GEMINI_API_KEY ?? '',
+  firestoreEmulator: process.env.FIRESTORE_EMULATOR_HOST ?? '',
 };
 
 export default config;
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,9 +2,11 @@ import { initializeApp } from 'firebase-admin/app';
 import admin from 'firebase-admin';
 
 import serviceAccount from '../../serviceAccountKey.json' with { type: 'json' };
+import config from './config.ts';
 
 const { credential, firestore } = admin;
 const cert = credential.cert(serviceAccount as admin.ServiceAccount);
+
 initializeApp({
   credential: cert,
   // Your bucket name looks like: your-project-id.firebasestorage.app
@@ -12,3 +14,7 @@ initializeApp({
 });
 
 export const db = firestore();
+
+if (config.nodeEnv === 'development') {
+  db.settings({ host: config.firestoreEmulator, ssl: false });
+}
