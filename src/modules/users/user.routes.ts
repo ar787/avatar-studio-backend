54bb9920424a4960a0edc4fa4c6637936c9eedd1
ar787 <armanhovhannisyan1002@gmail.com>
2026-04-20 12:48:59 +0400

    refactor(upload): implement file filter for image validation

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -2,11 +2,19 @@ import { Router } from 'express';
 import multer from 'multer';
 import * as userController from './user.controller.js';
 import { verifyAuthTokenHandler } from '../../middlewares/verifyAuthToken.middleware.js';
+import { BadRequestError } from '../../utils/errors/ApiErrors.js';
 
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
     fileSize: 3 * 1024 * 1024,
+  },
+  fileFilter(_, file, callback) {
+    if (file.mimetype.startsWith('image/')) {
+      callback(null, true);
+    } else {
+      callback(new BadRequestError('Only images are allowed!'));
+    }
   },
 });
 
